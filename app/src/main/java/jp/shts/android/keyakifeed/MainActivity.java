db4b395bfc2e9a3c90607f67f6c86052de218036
shts <shts.dev@gmail.com>
2015-12-03 22:28:38 +0900

    refactor

--- app/src/main/java/jp/shts/android/keyakifeed/MainActivity.java
+++ app/src/main/java/jp/shts/android/keyakifeed/MainActivity.java
@@ -2,10 +2,6 @@ package jp.shts.android.keyakifeed;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-
-import jp.shts.android.keyakifeed.activities.AllFeedListActivity;
-import jp.shts.android.keyakifeed.activities.AllMemberActivity;
 import jp.shts.android.keyakifeed.activities.TopActivity;
 
 
@@ -14,6 +10,5 @@ public class MainActivity extends TopActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
     }
 }
